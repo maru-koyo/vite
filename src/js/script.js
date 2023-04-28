64d71fe4bcaf2f962e50f4703ac91aabde8b014b
k.takaya <k.takaya@marukinad.co.jp>
2023-04-28 13:44:55 +0900

    delete csscomb

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,13 @@ $('.hello').on('click', function () {
 const huruine = '111';
 
 console.log(huruine);
+
+const array = [1, 2, 3, 4, 5];
+
+array.forEach((item) => {
+  console.log(item);
+  console.log(array + 'px');
+});
+
+const cibst = 255;
+console.log(cibst);
